2a32fc2a9de7c9ca24a9c0359c645628eb6f946c
thegree <32637287+thegree@users.noreply.github.com>
2019-05-12 12:30:55 +0300

    Update AdafruitPseudoThermin_pitches_test.ino

--- AdafruitPseudoThermin_pitches_test.ino
+++ AdafruitPseudoThermin_pitches_test.ino
@@ -1,51 +1,51 @@
 /*
-Adafruit Arduino - Lesson 10. Pseudo Thermin
+  Adafruit Arduino - Lesson 10. Pseudo Thermin
 */
 #include "pitches.h"
 
-//A piezo speaker connected to digital pin 12
+//A piezo speaker connected to digital pin 5
 int speakerPin = 12;
 
-//A stretch resistor connected to analog pin 0
+//A stretch resistor connected to analog pin A0
 int resistorPin = 0;
 
-int sensorTrip = 100; 
+int sensorTrip = 100;
 
 //The range of sensor values to be
 //mapped to music tones. 
-int lowerThreshold=80;
-int upperThreshold=900;
+int lowerThreshold = 80;
+int upperThreshold = 900;
 
 //Define the lowest and highest tone. Defined
 //in pitches.h
-int toneLow=NOTE_C3;
-int toneHigh=NOTE_B6;// Change sensor sensitivity here
+int toneLow = NOTE_C3;
+int toneHigh = NOTE_B6; // Change sensor sensitivity here
 
 void setup()
 {
-  //nothing to do here
+  Serial.begin(9600);
 }
 
 void loop()
 {
-int reading = analogRead(resistorPin);
+  int reading = analogRead(resistorPin);
+  Serial.println(reading);
 
 
-  
   //map the sensor value to tones. The map()
   //function maps values smaller than lower
   //threshold to the lowest desired value.
 
   int music;
-  if(reading>0){
-    music=map(reading,lowerThreshold,upperThreshold,toneLow,toneHigh);
-  }else{
-    music=0;
-  }   
-    if (reading > sensorTrip){               
-        tone(12, music, 10);                  // drive a piezo element at pin A, frequency B, and for C microseconds  
-    }
-    
-  
+  if (reading > 0) {
+    music = map(reading, lowerThreshold, upperThreshold, toneLow, toneHigh);
+  } else {
+    music = 0;
+  }
+  if (reading > sensorTrip) {
+    tone(speakerPin, music, 100);                  // drive a piezo element at pin A, frequency B, and for C microseconds
+  }
+
+  delay (1000);
 
 }
